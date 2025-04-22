347c49e967b0f4f668e2cd63c514ad0aa14f8444
Eloiza <eloizaeunice25@gmail.com>
2025-04-21 22:32:56 -0400

    fix: retirando a responsabilidade de enviar os exercicios do service

--- services/telegramService.js
+++ services/telegramService.js
@@ -5,21 +5,20 @@ dotenv.config()
 
 
 // Essa funcao que comeca o processo e eu quero que ela rode no horario que eu disser
-export async function enviarMensagemTelegram() {
+export async function enviarMensagemTelegram(mensagem) {
     //  O BOT ENVIA UMA MENSAGEM PRIMEIRO PERGUNTANDO QUAL O NIVEL QUE A PESSOA QUER
-    const message = "Olá, vamos para mais um exercício? Me diz qual o nível que você deseja praticar hoje -> Fácil, Difícil ou Médio?"
-
     // a mensagem do bot precisa ser enviada ao telegram através dessa URL
     const TELEGRAM_URL = `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`
     try {
         const mensagemASerEnviada = axios.post(TELEGRAM_URL, {
-            "chat_id": process.env.CHAT_ID,
-            "text": message
+            chat_id: process.env.CHAT_ID,
+            text: message
         })
-        console.log("✅ Mensagem enviada com sucesso:", response.data);
-
+        console.log("✅ Mensagem enviada com sucesso:", mensagemASerEnviada)
 
     } catch (error) {
         console.log(error)
     }
+
 }
+
