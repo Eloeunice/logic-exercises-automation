5582759ed3b2cad8119db0c1e3e8e1027dae23c3
Eloiza <eloizaeunice25@gmail.com>
2025-04-21 22:30:43 -0400

    config: configurando webhook

--- controllers/exerciseController.js
+++ controllers/exerciseController.js
@@ -1,29 +1,8 @@
-import { gerarExercicio } from "../services/openaiService.js"
 import Exercicio from "../models/Exercise.js"
 import { StatusCodes } from 'http-status-codes'
 
 
 // Funcao que pede o prompt (o prompt tem que ser personalizado de acordo com a dificuldade passada ( via vir da requisição do usuario?))
-
-export async function pedirNovoExercicio(req, res) {
-    // Pegar o nível de dificuldade da requisição enviado pelo usuario no TELEGRAM
-
-    try {
-        //Mandar para o service // Receber o exercicio do service e criar no banco
-        const { dificuldade } = req.body
-        const novoExercicio = await gerarExercicio(dificuldade) // espera o exercicio ser criado
-        const exercicioSalvo = await Exercicio.create(novoExercicio) // espera o exercicio criado ser salvo
-        console.log("Exercio salvo no banco com sucessso")
-        // retorna o exercicio
-        return res.status(StatusCodes.CREATED).json(exercicioSalvo);
-
-    } catch (error) {
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("Ocorreu um erro aqui!")
-        console.log(error)
-    }
-
-
-}
 
 export async function listarExercicios(req, res) {
     // Pegar os exercicios pelos status  que vem na requisicao
---
+++ controllers/telegramController.js
@@ -0,0 +1,50 @@
+import Exercicio from "../models/Exercise.js"
+import { gerarExercicio } from "../services/openaiService.js"
+import dotenv from "dotenv"
+import axios from "axios"
+import { enviarMensagemTelegram } from "../services/telegramService.js"
+
+dotenv.config()
+
+// WEBHOOK AVISA QUE UMA MENSAGEM FOI RECEBIDA
+
+export async function telegramWebhook(req, res) {
+    console.log("🔥 Webhook disparado!")
+
+    const mensagem = req.body.message?.text // PEGO A MENSAGEM DO TEXTO DIZENDO A DIFICULDADE
+    const chatID = req.body.message?.chat.id
+
+    console.log("Mensagem Recebida: ", mensagem);
+    console.log("Chat ID: ", chatID)
+
+    if (!mensagem || !chatID) {
+        return res.sendStatus(200); // Ignora mensagens que não são texto
+    }
+
+    if (mensagem.toLowerCase() === "/start") {
+        await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+            chat_id: chatID,
+            text: "Olá, vamos para mais um exercício? Me diz qual o nível que você deseja praticar hoje -> Fácil, Médio ou Difícil?"
+        });
+        return res.sendStatus(200)
+    }
+
+    try {
+        // CHAMO A GERAR EXERCICIO PASSANDO A DIFICULDADE (QUE É O TEXTO DÁ MENSAGEM)
+        const novoExercicio = await gerarExercicio(mensagem)
+        const exercicioSalvo = await Exercicio.create(novoExercicio)
+
+        // O EXERCICIO É ENVIADO PARA O USUARIO ATRAVÉS DO TELEGRAM
+        await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+            chat_id: chatID,
+            text: exercicioSalvo.Pergunta
+
+        })
+        return res.sendStatus(200)
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send("Erro ao processar a mensagem")
+    }
+
+}
