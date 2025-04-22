92c4e5d1761abc038c0d3e04a9af45733053da3b
Eloiza <eloizaeunice25@gmail.com>
2025-04-21 22:33:52 -0400

    feat: acrescentando rotas do webhook e configurando ngrok

--- routes/exerciseRoutes.js
+++ routes/exerciseRoutes.js
@@ -1,9 +1,8 @@
 import { Router } from "express"
-import { pedirNovoExercicio, listarExercicios } from "../controllers/exerciseController.js"
+import { listarExercicios } from "../controllers/exerciseController.js"
 
 const exerciseRoutes = Router()
 
 exerciseRoutes.get('/', listarExercicios)
-exerciseRoutes.post('/', pedirNovoExercicio)
 
 export default exerciseRoutes
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,12 @@
 // Armazena todas as rotas da aplicação
-
 import { Router } from "express"
 import exerciseRoutes from "./exerciseRoutes.js"
+import { telegramWebhook } from "../controllers/telegramController.js"
+import { enviarMensagemTelegram } from "../services/telegramService.js"
 
 const routes = Router()
 
 routes.use('/exercicio', exerciseRoutes)
+routes.post('/webhook', telegramWebhook)
 
 export default routes
