31cbc7b61d8124d8b355567a362acf9058ee1b7c
Eloiza <eloizaeunice25@gmail.com>
2025-04-28 23:32:40 -0400

    config: mudando banco de dados para postgres e fazendo conexao

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,11 +1,20 @@
-import mongoose from "mongoose"
+import { Sequelize } from "sequelize"
 import dotenv from "dotenv"
 
 dotenv.config()
 
+export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    dialect: `postgres`
+})
+
 async function conectaNoDatabase() {
-    await mongoose.connect(process.env.STRING_DB_CONNECTION, { dbName: "automation-exercises" })
-    return console.log("Conexão bem sucedida com o banco de dados")
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
 
 }
 
