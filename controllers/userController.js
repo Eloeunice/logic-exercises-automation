ee40c52d9aeebcdcd00a682502e4682bc9e341db
Eloiza <eloizaeunice25@gmail.com>
2025-04-29 23:45:51 -0400

    fix: lida com os erros http

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+import { ZodError } from "zod"
 import { userSchema } from "../middlewares/validationMiddleware.js"
 import User from "../models/user.js"
 
@@ -7,12 +8,15 @@ export async function registerUser(req, res) {
         const { email, username, password } = req.body
         // os dados vao para o zod para serem validados
         const validateData = userSchema.parse({ email, username, password })
-
         // case de tudo certo ele vai para o useCase para criar o usuario
         const newUser = await User.create(validateData)
-        console.log(newUser)
+        res.status(201).send({ message: "Usuário criado com sucesso" })
+
     } catch (error) {
-        res.status(500).send('Erro aqui')
+        if (error instanceof ZodError) {
+            console.log("Erro de validação!")
+        }
+        res.status(400).send('Houve um erro ao processar os dados. Tente novamente!')
         console.log(error)
     }
 }
