c8f2cda5f25d4ed6e7b3fba45d278aa7fc55fcb0
Eloiza <eloizaeunice25@gmail.com>
2025-04-29 23:34:43 -0400

    feat: cria controller para registro de novo user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,18 @@
+import { userSchema } from "../middlewares/validationMiddleware.js"
+import User from "../models/user.js"
 
+export async function registerUser(req, res) {
+    try {
+        // receber email, username e password pela requisicao
+        const { email, username, password } = req.body
+        // os dados vao para o zod para serem validados
+        const validateData = userSchema.parse({ email, username, password })
 
-export async function registerUser(params) {
-    // receber email, username e password e criar no banco usando useCase
+        // case de tudo certo ele vai para o useCase para criar o usuario
+        const newUser = await User.create(validateData)
+        console.log(newUser)
+    } catch (error) {
+        res.status(500).send('Erro aqui')
+        console.log(error)
+    }
 }
