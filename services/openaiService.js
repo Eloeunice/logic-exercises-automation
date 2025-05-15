2c5d1445f1d57dae11c9e346feba9efdd6913834
Eloiza <eloizaeunice25@gmail.com>
2025-05-15 15:29:04 -0400

    feat: envia resposta do usuário para o chatgpt gerar feedback

--- services/openaiService.js
+++ services/openaiService.js
@@ -45,5 +45,46 @@ Responda somente em JSON no seguinte formato:
     }
 }
 
+export async function gerarFeedback({ question, response }) {
+    // enviar a resposta junto com a pergunta que o usuario esta respondendo
+    const prompt = ` Corrija a resposta do exercício considerando a estrutura a ser armazenada no banco de dados.
+    A resposta deve ser clara e diretamente relacionada à questão proposta.
+
+        Pergunta: ${question}
+        Resposta: ${response}
+        
+    Avalie os seguintes critérios:
+
+    - "feat": nota de 1 a 10 indicando o quão próxima a resposta está da correta (sendo 10 totalmente correta).
+    - "is_correct": considere **true** se o feat for alto o suficiente para considerar a resposta correta.
+    - "comment": explique brevemente o que está incorreto e dê uma sugestão clara do que melhorar ou revisar. O comentário **não deve ser longo**.
+    Retorne apenas em formato JSON, exatamente conforme o modelo abaixo:
+    {
+    "feat": { "type": "DataTypes.INTEGER" },
+    "is_correct": { "type": "DataTypes.BOOLEAN" },
+    "comment": { "type": "DataTypes.TEXT" }
+    }`
+
+    try {
+        const resposta = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages: [{ role: "user", content: prompt }],
+            temperature: 0.7,
+        });
+
+        const conteudo = resposta.choices[0].message.content
+
+        // Garantir que está vindo JSON válido
+        const feedback = JSON.parse(conteudo)
+        console.log(feedback)
+        return feedback;
+
+    } catch (erro) {
+        console.error("Erro ao chamar a OpenAI:", erro);
+        throw erro;
+    }
+}
+
+
 
 
