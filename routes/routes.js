b5fdde43ad58e148e7e3d5d00ca0635c9e0dac86
Eloiza <eloizaeunice25@gmail.com>
2025-05-26 15:33:15 -0400

    feat: acrescenta rotas da dashboard

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,7 @@ import exerciseRoutes from "./exerciseRoutes.js"
 import authRoutes from "./authRoutes.js"
 import answerRoutes from "./answerRoutes.js"
 import examRoutes from "./examRoutes.js"
+import dashboardRoutes from "./dashboardRoutes.js"
 import { verifyToken } from "../middlewares/passport.js"
 // import { telegramWebhook } from "../controllers/telegramController.js"
 
@@ -14,6 +15,7 @@ routes.use('/', authRoutes)
 routes.use('/exercicios', verifyToken, exerciseRoutes) // rota, middleware, controller
 routes.use('/respostas', verifyToken, answerRoutes)
 routes.use('/provas', verifyToken, examRoutes)
+routes.use('/metricas', verifyToken, dashboardRoutes)
 
 // routes.post('/webhook', telegramWebhook)
 
