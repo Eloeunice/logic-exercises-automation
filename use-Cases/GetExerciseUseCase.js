47cc9762efd0fc5c46fd9a7a040c2528d746ce1b
Eloiza <eloizaeunice25@gmail.com>
2025-05-31 19:41:04 -0400

    fix: altera os parametros do use case de exercicio para relacionar o user ao exercicio quando este for criado

--- use-Cases/GetExerciseUseCase.js
+++ use-Cases/GetExerciseUseCase.js
@@ -1,12 +1,16 @@
 import Exercises from "../models/exercise.js"
 import { gerarExercicio } from "../services/openaiService.js"
 // recebe o req.body  e o modelo
-export async function getExercise({ difficulty }, exerciseModel) {
+export async function getExercise({ difficulty }, userId) {
     // recebe os parametros que o controller vai ter enviado
     // manda pro service e recebe
     const newExercise = await gerarExercicio(difficulty)
+    const newExerciseWithUser = {
+        ...newExercise,
+        userId: userId
+    };
     // conversa com o banco para criar o novo exercicio e retorna
-    const exercicio = await Exercises.create(newExercise)
+    const exercicio = await Exercises.create(newExerciseWithUser)
     console.log(difficulty)
     return exercicio
 
