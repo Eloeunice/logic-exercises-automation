73d81bea377c23309c6a1fbb3fa2e67165e25a80
Eloiza <eloizaeunice25@gmail.com>
2025-06-11 11:30:59 -0400

    docs: implementa arquivo de documentacao com swagger

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ import express from "express"
 import dotenv from "dotenv"
 import conectaNoDatabase from "./config/dbConnect.js"
 import routes from "./routes/routes.js"
+import swaggerUI from "swagger-ui-express"
+import fs from "fs/promises";
+
+const jsonText = await fs.readFile("./swagger.json", "utf-8");
+const swaggerDocument = JSON.parse(jsonText);
 
 dotenv.config()
 const app = express()
 
 app.use(express.json()) // servidor processar requisições json
 app.use("/", routes)
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 
 const PORT = 3000
